f32134c5f93f3c49c0d35a516e08aa7ce19055ca
naimaaan <Askarov_Z@list.ru>
2024-10-28 07:05:51 +0600

    Calendar.tsx created

--- frontend/astar/src/App.tsx
+++ frontend/astar/src/App.tsx
@@ -1,18 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
-import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
-import Header2 from "./components/Header2";
+import Calendar from "./pages/Calendar";
+
 function App() {
   return (
     <Router>
+
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/sidebar" element={<Sidebar />}></Route>
-        <Route path="/header" element={<Header />}></Route>
-        <Route path="/header2" element={<Header2 />}></Route>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Calendar" element={<Calendar />} />
       </Routes>
+      
     </Router>
   );
 }
--- frontend/astar/src/main.js
+++ frontend/astar/src/main.js
@@ -1,3 +1,4 @@
+import { jsx as _jsx } from "react/jsx-runtime";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -5,9 +6,4 @@ import App from "./App";
 import './app.css';
 import './css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(_jsx(StrictMode, { children: _jsx(App, {}) }));
