998f77c114780e14da238e975a623648eca980b8
BekzhanK1 <bkimadieff@gmail.com>
2024-11-03 22:53:43 +0500

    add: docker

--- backend/astar_backend/urls.py
+++ backend/astar_backend/urls.py
@@ -24,7 +24,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    # path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
