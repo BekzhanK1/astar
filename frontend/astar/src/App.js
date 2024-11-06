7ca18388b37f3ef3cd8c21050394652a83858813
BekzhanK1 <bkimadieff@gmail.com>
2024-11-07 03:39:44 +0500

    delete front

--- frontend/astar/src/App.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import Calendar from "./pages/Calendar";
-function App() {
-    return (_jsx(Router, { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/Login", element: _jsx(Login, {}) }), _jsx(Route, { path: "/Calendar", element: _jsx(Calendar, {}) })] }) }));
-}
-export default App;
--- frontend/astar/src/main.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import './app.css';
-import './css/style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-createRoot(document.getElementById("root")).render(_jsx(StrictMode, { children: _jsx(App, {}) }));
